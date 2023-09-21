62d43feff8c23351240a0f1743b10308098c4d6c
Ian Gardocki <ian.d.gardocki@gmail.com>
2023-09-20 21:36:29 -0500

    added restart button

--- src/End.jsx
+++ src/End.jsx
@@ -1,11 +1,18 @@
-import { Container, Card } from "react-bootstrap";
+import { Card, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 export const End = () => {
+    const navigate = useNavigate();
     return (
-         <Card>
+        <>
+                 <Card>
                 <Card.Body>
                     You and your parent or other trusted adult return home after a FANTASTIC night of trick-or-treating. They check your candy to make sure it's safe, and then you watch your favorite halloween movie together. What a memorable time! Thank you for joining us on this halloween adventure!
                 </Card.Body>
-            </Card>
+                <Button onClick={() => navigate('/')}>Restart The Adventure!</Button>
+        </Card>
+
+        </>
+
     )
 }
